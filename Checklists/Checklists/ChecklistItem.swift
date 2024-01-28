9582f2ac4711cc76976c1a00e31cebcf395638a9
CQuach123 <156718368+CQuach123@users.noreply.github.com>
2024-01-27 23:57:30 -0500

    Add files via upload

---
+++ Checklists/Checklists/ChecklistItem.swift
@@ -0,0 +1,15 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Chi Quach on 1/27/24.
+//
+
+import Foundation
+
+class ChecklistItem: NSObject {
+    
+    var text = ""
+    var checked = false
+    
+}
